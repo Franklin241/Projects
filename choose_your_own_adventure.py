4b74938a04bcd2e1f85d654496f419168023c727
Franklin Hepburn Jr <144951539+Franklin241@users.noreply.github.com>
2024-02-20 14:36:59 -0500

    Update choose_your_own_adventure.py

--- choose_your_own_adventure.py
+++ choose_your_own_adventure.py
@@ -10,7 +10,7 @@ if answer == "left":
     if answer == "continue":
         print("You treak onwards to the village but upon entering the village you notice that something is wrong its quiet and desolite. Not a living, breathing soul in sight, you investage futher in and find a hord of the undead and then was eaten alive joinig their ranks.")
     else:
-        print('Not a valid option. Am sorry but your soul has been sent to the great beyond. ')
+        print('Not a valid option. Am sorry but your soul has been sent to the great beyond.')
 
 elif answer == "right":
     answer = input("You take the path to the right and contiune walking up a mountainess path. The wind sundenly picks up forcing you to treak up the mountain faster. You make it too the top of the mountain and see a place to take shelter. Do you take shelter or continue running in search of a better one (take shelter/ continue on)? ")
@@ -25,8 +25,10 @@ elif answer == "right":
         elif answer == "no":
             print("You ignore this strangely dress person and they are offended and you lose the game.")
         else:
-            print('Not a valid option. Am sorry but your soul has been sent to the great beyond. ')  
+            print('Not a valid option. Am sorry but your soul has been sent to the great beyond.')  
     else:
-        print('Not a valid option. Am sorry but your soul has been sent to the great beyond. ')
+        print('Not a valid option. Am sorry but your soul has been sent to the great beyond.')
 else:
-    print('Not a valid option. Am sorry but your soul has been sent to the great beyond. ')
+    print('Not a valid option. Am sorry but your soul has been sent to the great beyond.')
+
+print("Thank you for trying the game", name)
